8796e3ae7a2603faee1fad1507d01477108dc296
Lyutova <32490821+Lyutova@users.noreply.github.com>
2022-09-11 17:50:23 +0500

    Изменены настройки форматирования

--- sandbox/src/main/java/lu/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/lu/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package lu.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[]args) { System.out.println("Hello, world!");
-
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
     }
 }
