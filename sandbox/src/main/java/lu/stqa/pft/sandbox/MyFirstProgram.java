0aea94e2817f34f3841b6c2671643e1f46c00268
Lyutova <32490821+Lyutova@users.noreply.github.com>
2022-10-23 16:15:52 +0500

    Добавлены функции

--- sandbox/src/main/java/lu/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/lu/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,24 @@ package lu.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello(" Yulia");
+
+        double l =5;
+        System.out.println("Площаль квадрата " + l + " = " + area(l));
+
+        double t=2;
+        double r=3;
+        System.out.println("Площаль прямоуголька со сторанами  " + t + " и " + r + " = " + area(t,r));
     }
+    public static void hello (String somebody){
+        System.out.println("Hello,"+ somebody +"!");
+    }
+    public static double area(double len) {
+        return len*len;
+    }
+    public static double area(double a,double b) {
+        return a*b;
+    }
+
 }
+
