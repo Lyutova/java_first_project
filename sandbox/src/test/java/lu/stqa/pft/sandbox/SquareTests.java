2492801e2478c98886d9938539c5b39b4bc1dd4f
Lyutova <32490821+Lyutova@users.noreply.github.com>
2022-11-04 03:31:29 +0500

    Завершен 2 урок,
    Завершен 2 урок, Первый тест по вычислению площади квадрата

--- sandbox/src/test/java/lu/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/lu/stqa/pft/sandbox/SquareTests.java
@@ -1,8 +1,15 @@
 package lu.stqa.pft.sandbox;
 
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SquareTests {
-  public void testArea(){
-    Square s = new Square();
+ @Test
+  public void testArea() {
+
+    Square s = new Square(5);
+    Assert.assertEquals( s.area(),25);
 
   }
 }
